73c01832129c542f493e61ef8888469e38aac27e
Nicholas <nikhsd899@outlook.com>
2021-11-12 11:15:45 -0800

    submission for deadline

--- src/index.js
+++ src/index.js
@@ -1,1 +1,30 @@
-// write your code here
+const baseUrl = 'http://localhost:3000/images/1'
+
+fetch
+
+document.getElementById("card-image").src = "./assets/coder-dog.png";
+
+
+
+"id": 1,
+"title": "Woofing those bugs away",
+"likes": 0,
+"image": "./assets/coder-dog.png",
+"comments": [
+  {
+    "id": 1,
+    "imageId": 1,
+    "content": "What a cute dog!"
+  },
+  {
+    "id": 2,
+    "imageId": 1,
+    "content": "He's got a nose for bugs!"
+  },
+  {
+    "id": 3,
+    "imageId": 1,
+    "content": "Woof!"
+  }
+]
+}
